d66afe4b036c20fab24093686496c5b9b48482c6
Amine Bouzayan <aminebouzayan10@gmail.com>
2021-12-22 13:16:21 +0100

    added files

---
+++ pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contact = () =>
+{
+  return (
+    <>
+      <div className={'bg-warning ps-2'}>
+        <p>lorem ipsum dolor set ame</p>
+        <ol>
+          <li>yop</li>
+          <li>yop</li>
+          <li>yop</li>
+        </ol>
+      </div>
+      <section>
+        <button className='btn'>remind</button>
+        <span>I'll never forget this days</span>
+      </section>
+    </>
+  )
+}
+
+export default Contact
