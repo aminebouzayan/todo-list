a7a4ced6d2578c902e0043de71c3f9210c117e69
Amine Bouzayan <aminebouzayan10@gmail.com>
2021-12-23 11:23:21 +0100

    added file

---
+++ components/Carousel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Carousel = () =>
+{
+  return (
+    <section className='carousel'>
+      <div className='container-xxl'>
+        <span className="badge bg-..."></span>
+      </div>
+      <div className='carousel-controls'></div>
+      <ul className='carousel-indicators'></ul>
+    </section>
+  )
+}
+
+export default Carousel
