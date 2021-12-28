0988404744a1190fc1907705645fb82e20f0d1ec
Amine Bouzayan <aminebouzayan10@gmail.com>
2021-12-28 18:10:55 +0100

    added files

---
+++ components/dependencies/AccordionData.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    title: "what we are offering to people ?",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptatem dicta sed tempore veniam aliquid incidunt? Nesciunt, illum. Beatae aliquid provident numquam!",
+  },
+  {
+    id: 2,
+    title: "when we are available for responding ?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis quo nobis, sint, ipsa cumque autem corrupti, magnam velit voluptate porro quaerat vel?",
+  },
+  {
+    id: 3,
+    title: "why we are hopeful for you to join us ?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quasi doloribus praesentium placeat iste dolor? Eius atque soluta ea voluptatibus neque voluptatem!",
+  },
+  {
+    id: 4,
+    title: "where we are in MENA region ?",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum sequi veniam odit quos fugiat quis. Maxime magnam repellat deleniti totam nobis aliquam.",
+  },
+  {
+    id: 5,
+    title: "how we are making internet so fast ?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus architecto, suscipit a illum illo at cum quidem vero. Magnam, vel repudiandae!",
+  },
+];
