fccf9565b7266f08b60fa3f55c6784f4a3aeafc5
Amine Bouzayan <aminebouzayan10@gmail.com>
2022-01-04 13:58:10 +0100

    deleted files

--- pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Accordion from "../components/Accordion";
-// import Carousel from "../components/Carousel";
-
-const Contact = () => {
-  return <Accordion />
-};
-
-export default Contact;
--- pages/todo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Tasks from "../components/Tasks";
-
-const todo = () => {
-  return <Tasks />;
-};
-
-export default todo;
